934b706a843788658cddd017eb1457a8dbefef5e
Ondřej Vacek <ondrej.vacek@entry-cz.com>
2023-05-31 15:06:06 +0200

    Add Threading to Simple Tracker

--- SimpleTracker/main.cpp
+++ SimpleTracker/main.cpp
@@ -3,73 +3,129 @@
 #include <numeric>
 #include <chrono>
 
-void initCamera();
-void draw_cross_relative(cv::Mat& img, cv::Point2f center_relative, int size);
-cv::Point2f findCenterRelative(cv::Mat& frame);
-cv::Point2f findCenterRelativeHSV(cv::Mat& frame, cv::Scalar lowerBound, cv::Scalar upperBound, bool vizualizeFoundPixels);
+struct CameraData {
+    cv::Point2f center;
+    cv::Mat img;
+};
 
-typedef struct s_globals {
-    cv::VideoCapture capture;
-} s_globals;
+void initializeCamera();
+void releaseCamera();
 
-s_globals globals;
+void processCamera();
+cv::Point2f findCenterRelativeHSV(cv::Mat& frame, cv::Scalar lowerBound, cv::Scalar upperBound);
+
+void drawCrossRelative(cv::Mat& img, cv::Point2f center_relative, int size);
+
+//HSV range (0..180) (0..255) (0..255)
+cv::Scalar lowerBound(40, 100, 50);
+cv::Scalar upperBound(80, 255, 255);
+
+cv::VideoCapture videoCapture;
+std::mutex cameraMutex;
+std::unique_ptr<CameraData> sharedCameraData;
 
 int main()
 {
-    cv::Mat frame, scene;
+    initializeCamera();
 
-    initCamera();
+    //Start camera processing thread
+    std::thread cameraThread = std::thread(processCamera);
 
-    //HSV range (0..180) (0..255) (0..255)
-    cv::Scalar lowerBound(40, 100, 50);
-    cv::Scalar upperBound(80, 255, 255);
+    cv::namedWindow("Camera");
 
-    while (true)
+    std::unique_ptr<CameraData> localCameraData;
+    bool newCameraData = false;
+    bool isRunning = true;
+    while (videoCapture.isOpened() && isRunning)
     {
-        globals.capture.read(frame);
-        if (frame.empty())
+        //Receive data from camera processing thread
+        cameraMutex.lock();
+        if (sharedCameraData && !sharedCameraData->img.empty())
         {
-            std::cerr << "device disconected or video ended" << std::endl;
-            break;
+            localCameraData = std::move(sharedCameraData);
+            newCameraData = true;
         }
-	    
-        auto center = findCenterRelativeHSV(frame, lowerBound, upperBound, true);
+        cameraMutex.unlock();
 
-	    draw_cross_relative(frame, center, 25);
-
-        cv::namedWindow("frame");
-        cv::imshow("frame", frame);
-
-        cv::waitKey(1); //poll OS events
+       if (newCameraData)
+        {
+            std::cout << "\033[K\rDetected center: " << localCameraData->center; //clear line and print
+            drawCrossRelative(localCameraData->img, localCameraData->center, 25);
+            cv::imshow("Camera", localCameraData->img);
+        }
+	   
+        newCameraData = false;
+        char key = cv::waitKey(16); //poll OS events every 16ms (cca 60 fps)
+        switch (key)
+        {
+            case 27: // Escape key
+                isRunning = false;
+                break;
+        }
     }
 
-    if (globals.capture.isOpened())
-        globals.capture.release();
+    releaseCamera();
+    cameraThread.join();
+
+    return EXIT_SUCCESS;
 }
 
-void initCamera()
-{
-    globals.capture = cv::VideoCapture(cv::CAP_DSHOW);
 
-    if (!globals.capture.isOpened())
+void initializeCamera()
+{
+    videoCapture = cv::VideoCapture(cv::CAP_DSHOW);
+    if (!videoCapture.isOpened())
     {
         std::cerr << "no camera" << std::endl;
         exit(EXIT_FAILURE);
     }
-    else
+
+    std::cout << "Camera " <<
+        ": width=" << videoCapture.get(cv::CAP_PROP_FRAME_WIDTH) <<
+        ", height=" << videoCapture.get(cv::CAP_PROP_FRAME_HEIGHT) <<
+        ", FPS=" << videoCapture.get(cv::CAP_PROP_FPS) << std::endl;
+}
+
+void releaseCamera()
+{
+    if (videoCapture.isOpened())
+        videoCapture.release();
+}
+
+
+void processCamera()
+{
+    while (videoCapture.isOpened())
     {
-        std::cout << "Camera " <<
-            ": width=" << globals.capture.get(cv::CAP_PROP_FRAME_WIDTH) <<
-            ", height=" << globals.capture.get(cv::CAP_PROP_FRAME_HEIGHT) <<
-            ", FPS=" << globals.capture.get(cv::CAP_PROP_FPS) << std::endl;
+        cv::Mat frame;
+        videoCapture.read(frame);
+        if (frame.empty())
+        {
+            std::cerr << "device disconected or video ended" << std::endl;
+            releaseCamera();
+            break;
+        }
+
+        auto tmpCameraData = std::make_unique<CameraData>();
+        tmpCameraData->center = findCenterRelativeHSV(frame, lowerBound, upperBound);
+        frame.copyTo(tmpCameraData->img);
+
+        //Move data to shared variable
+        cameraMutex.lock();
+        sharedCameraData = std::move(tmpCameraData);
+        cameraMutex.unlock();
+
+        //std::this_thread::sleep_for(std::chrono::milliseconds(5));   //Sleep for 5ms
     }
 }
 
-cv::Point2f findCenterRelativeHSV(cv::Mat& frame, cv::Scalar lowerBound, cv::Scalar upperBound, bool vizualizeFoundPixels = false)
+
+cv::Point2f findCenterRelativeHSV(cv::Mat& frame, cv::Scalar lowerBound, cv::Scalar upperBound)
 {
     cv::Mat frameHsv;
     cv::cvtColor(frame, frameHsv, cv::COLOR_BGR2HSV);
 
+    // Tresholding
     cv::Mat frameTresholded;
     cv::inRange(frameHsv, lowerBound, upperBound, frameTresholded);
 
@@ -78,62 +134,29 @@ cv::Point2f findCenterRelativeHSV(cv::Mat& frame, cv::Scalar lowerBound, cv::Sca
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
     cv::morphologyEx(frameTresholded, frameCleared, cv::MORPH_OPEN, kernel);
 
-    if(vizualizeFoundPixels)
-        cv::namedWindow("frameTrehsolded");
-        cv::imshow("frameTrehsolded", frameCleared);
-
+    // Calculating center of mass
     std::vector<cv::Point> whitePixels;
     cv::findNonZero(frameCleared, whitePixels);
-    cv::Point whiteReduced = std::reduce(whitePixels.begin(), whitePixels.end());
+    cv::Point whiteReduced = std::reduce(whitePixels.begin(), whitePixels.end()); //sum of all points per axis
+    cv::Point2f center((float)whiteReduced.x / whitePixels.size() , (float)whiteReduced.y / whitePixels.size());
+    cv::Point2f centerRelative(center.x / frame.cols, center.y / frame.rows);
 
-    cv::Point2f centerRelative((float)whiteReduced.x / whitePixels.size() / frame.cols, (float) whiteReduced.y / whitePixels.size() / frame.rows);
     return centerRelative;
 }
 
-cv::Point2f findCenterRelative(cv::Mat& frame)
+
+void drawCrossRelative(cv::Mat& img, cv::Point2f centerRelative, int size)
 {
-    cv::Mat gray_frame;
-    frame.copyTo(gray_frame);
-
-    int sx = 0, sy = 0, sw = 0;
-    for (int i = 0; i < frame.cols; i++)
-    {
-        for (int j = 0; j < frame.rows; j++)
-        {
-            cv::Vec3b pixel = frame.at<cv::Vec3b>(j, i);
-            unsigned char Y = 0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0];
-
-            if (Y < 230)
-                Y = 0;
-            else {
-                Y = 255;
-                sx += i;
-                sy += j;
-                sw++;
-            }
-
-            gray_frame.at<cv::Vec3b>(j, i) = cv::Vec3b(Y, Y, Y);
-        }
-    }
-
-    cv::Point2i center(sx / sw, sy / sw);
-    cv::Point2f centerRelative((float)center.x / frame.cols, (float)center.y / frame.rows);
-
-    return centerRelative;    
-}
-
-void draw_cross_relative(cv::Mat& img, cv::Point2f center_relative, int size)
-{
-    center_relative.x = std::clamp(center_relative.x, 0.0f, 1.0f);
-    center_relative.y = std::clamp(center_relative.y, 0.0f, 1.0f);
+    centerRelative.x = std::clamp(centerRelative.x, 0.0f, 1.0f);
+    centerRelative.y = std::clamp(centerRelative.y, 0.0f, 1.0f);
     size = std::clamp(size, 1, std::min(img.cols, img.rows));
 
-    cv::Point2f center_absolute(center_relative.x * img.cols, center_relative.y * img.rows);
+    cv::Point2f centerAbsolute(centerRelative.x * img.cols, centerRelative.y * img.rows);
 
-    cv::Point2f p1(center_absolute.x - size / 2, center_absolute.y);
-    cv::Point2f p2(center_absolute.x + size / 2, center_absolute.y);
-    cv::Point2f p3(center_absolute.x, center_absolute.y - size / 2);
-    cv::Point2f p4(center_absolute.x, center_absolute.y + size / 2);
+    cv::Point2f p1(centerAbsolute.x - size / 2, centerAbsolute.y);
+    cv::Point2f p2(centerAbsolute.x + size / 2, centerAbsolute.y);
+    cv::Point2f p3(centerAbsolute.x, centerAbsolute.y - size / 2);
+    cv::Point2f p4(centerAbsolute.x, centerAbsolute.y + size / 2);
 
     cv::line(img, p1, p2, CV_RGB(255, 0, 0), 3);
     cv::line(img, p3, p4, CV_RGB(255, 0, 0), 3);
